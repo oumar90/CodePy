f263c70ef0d1f12dc612315509608a2b38befa62
oumar90 <oudjira90@gmail.com>
2018-05-05 20:12:04 +0100

    determination du code lieaire

--- alphabets.py
+++ alphabets.py
@@ -1,44 +1,88 @@
+import math
 #calcul d'entropie
 def entropie(n):
 	return (math.log2(n))
+
+def longueurmoy(c, n):
+	som = 0
+	j =0
+	while j < len(c):
+		som=som+len(i)*(1/n)
+		j+=1
+	return som	
+
 		
 #cacul de longueur minimum		
-def longueurmin(entropie):
+def longueurmin(entropie, n):
 	return entropie/math.log2(n)
 
 #calcul de rendement
 def rendement(longueurmin, longueurmoy):
 	return longueurmin/longueurmoy	
+
 	
-#pour stocker l'alphabet
-alphabet = ""
+def main():
+	#pour stocker l'alphabet
+	alphabet = ""
 
 
-alphabet1 = input("entrer l'alphabet :")
-alphabet = alphabet1
-#le code est composé d'un ensemble de mot separer par un space
-code = input("entrer une serie de mot separer par un space : ")
+	alphabet1 = input("entrer l'alphabet :")
+	print()
+	alphabet = alphabet1
+	#le code est composé d'un ensemble de mot separer par un space
+	code = input("entrer une serie de mot separer par un space : ")
+	print()
 
-#on transforme l'ensemble de code en une liste
-nouveau_code = code.split()
-print(nouveau_code)
+	#on transforme l'ensemble de code en une liste
+	nouveau_code = code.split()
+	print(nouveau_code)
+	print()
 
-drapeau = 1 #on declare un boolean pour test
+	drapeau = 1 #on declare un boolean pour test
+	global i
+	for i in nouveau_code: #on parcours toute la liste nouveau_code 
+		for j in i:        #on parcours chaque element du mot de la liste
+			if j not in alphabet: #on verifie est-ce que l'element n'appartient pas à l'alphabet
+				drapeau = 0	 #si oui on met drapeau à 0
+		for l in nouveau_code: #on parcours toute la liste nouveau_code
+			if i.startswith(l): #on determine si j est prefixé de i
+				if i == l: #Lorsque i=j on contine
+					continue
+				drapeau = 0
 
-for i in nouveau_code: #on parcours toute la liste nouveau_code 
-	for j in i:        #on parcours chaque element du mot de la liste
-		if j not in alphabet: #on verifie est-ce que l'element n'appartient pas à l'alphabet
-			drapeau = 0	 #si oui on met drapeau à 0
-	for l in nouveau_code: #on parcours toute la liste nouveau_code
-		if i.startswith(l): #on determine si j est prefixé de i
-			if i == l: #Lorsque i=j on contine
-				continue
-			drapeau = 0
+	dim = len(alphabet)
+	card = len(nouveau_code)
+	ento = entropie(card)
+	longMin = longueurmin(ento,card)
+	longMoy = longueurmoy(nouveau_code, card)
+	rend = rendement(longMin, longMoy)
+	redon = 1 - rend
+	if drapeau == 1: #si oui alors c'est un code 
 
-if drapeau == 1: #si oui alors c'est un code 
-	print("Le mot {} est un code".format(nouveau_code))
-			
-else: #sinon ce n'est pas un code
-	print("Le mot {} n'est un alphabet desolé !!! ".format(nouveau_code))
-	
+		print("Le mot {} est un code".format(nouveau_code))
+		print("Les caracteristiques sont:")
+		print("      |--*--> Entropie     : ", ento)
+		print("      |--*--> Longueur min : ", longMin)
+		print("      |--*--> Longueur moy : ", longMoy)
+		print("      |--*--> Rendement    : ", rend)
+		print("      |--*--> Redondance   : ", redon)
+		print("      |--*--> Dimension    : ", dim)
 
+		count=1
+		for i in nouveau_code:
+			if int(i) != 0:
+				count=0
+			else:
+				count=1
+		if count==1:		
+			print("      |--*--> Ce code {} est linéaire puisqu'il contient au moins un mot nul.".format(nouveau_code))
+		else:	
+			print("      |--*--> Ce code n'est pas linéaire par ce qu'il ne contient aucun mot null.")	
+
+	else: #sinon ce n'est pas un code
+		print("Le mot {} n'est un code, desolé !!! ".format(nouveau_code))
+		
+
+#---------------------Programme principal---------------------
+if __name__ == '__main__':
+	main()
